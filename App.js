27a1586563c8315690147ac4ef31e08450da6490
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-13 22:07:53 -0400

    create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, Pressable, View, StyleSheet} from 'react-native';
+
+
+export default function App () {
+    return (
+        <>
+        <Text>Oh HEYYYYY</Text>
+        </>
+    )
+}
