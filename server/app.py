79b1575be50fca40e12b2d14cabda184425f8054
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-19 12:07:13 -0400

    test home page set up for app.py successfully

--- server/app.py
+++ server/app.py
@@ -1,3 +1,15 @@
 from flask import request, session, make_response
 from flask_restful import Resource
-from config import app, db, Api
+from config import app, db, api
+
+class Home(Resource):
+    def get(self):
+        return make_response("YEEHAW")
+
+
+
+
+api.add_resource(Home, '/')
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
