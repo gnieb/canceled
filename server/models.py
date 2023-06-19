887fd6aceea7d64cf162a1bca4d63d052c511318
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-19 12:01:49 -0400

    user, plan , and puinstance models created

--- server/models.py
+++ server/models.py
@@ -10,12 +10,16 @@ from datetime import datetime
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
+    serialize_rules = ('-puinstances', '-plans')
+
     id = db.Column(db.Integer, primary_key = True)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     birth_year = db.Column(db.Integer, nullable=False)
     username = db.Column(db.String, nullable=False, unique=True)
     _password_hash = db.Column(db.String, nullable=False)
+    puinstances = db.relationship('PUInstance', backref='user' )
+    plans = association_proxy('puinstances', 'plan')
 
     @hybrid_property
     def password_hash(self):
@@ -33,10 +37,6 @@ class User(db.Model, SerializerMixin):
             self._password_hash, password.encode('utf-8'))
     
 
-
-
-
-
     # @validates('birth_year')
     # def validate_birth_year(self, key, year):
     #     today = datetime.now()
@@ -45,8 +45,23 @@ class User(db.Model, SerializerMixin):
 class Plan(db.Model, SerializerMixin):
     __tablename__ = 'plans'
 
+    serialize_rules = ('-puinstances','-users')
+
     id = db.Column(db.Integer, primary_key=True)
     start_time = db.Column(db.DateTime)
     end_time = db.Column(db.DateTime)
+    puinstances = db.relationship('PUInstance', backref='plan' )
+    users = association_proxy('puinstances', 'user')
+
+class PUInstance(db.Model, SerializerMixin):
+    # puinstance: plan-user instance, or one instance between a plan and a user
+    __tablename__ = 'puinstances'
+
+    serialize_rules = ('-user.puinstances','-plan.puinstances')
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    plan_id = db.Column(db.Integer, db.ForeignKey('plans.id'))
+
 
 
