bfd9b4876d089ab1abb5a5f5f3d3be5a4ac66fc6
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-19 12:17:10 -0400

    database created and head upgraded

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import request, session, make_response
 from flask_restful import Resource
 from config import app, db, api
+from models import Plan, User, PUInstance
 
 class Home(Resource):
     def get(self):
