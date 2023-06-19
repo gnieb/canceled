71550c4aba4d953709f295b30e85cfac54e6798a
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-19 11:42:19 -0400

    password auth methods set up

--- server/models.py
+++ server/models.py
@@ -14,8 +14,25 @@ class User(db.Model, SerializerMixin):
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     birth_year = db.Column(db.Integer, nullable=False)
-    username = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False, unique=True)
     _password_hash = db.Column(db.String, nullable=False)
+
+    @hybrid_property
+    def password_hash(self):
+        raise Exception('Password hashes may not be viewed')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        if len(password) < 8:
+            raise ValueError('Passwords must be 8 characters or more')
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+    
+
 
 
 
