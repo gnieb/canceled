e52b2a83ff54de974b22a141b1e1253355228382
Grace Nieboer <grace.nieboer@outlook.com>
2023-06-19 21:22:08 -0400

    navigation buttons work for calendar and profile

--- components/Calendar.js
+++ components/Calendar.js
@@ -4,7 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Calendar ({navigation}) {
 return (
     <SafeAreaView>
-        <Text>Hola</Text>
+        <Text>Hola, this is your calendar</Text>
     </SafeAreaView>
 )
 }
--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View} from 'react-native'
+import {Pressable, Text, View} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home ({navigation}) {
@@ -9,6 +9,12 @@ export default function Home ({navigation}) {
             <Text>Welcome To Canceled</Text>
             <Text>An Introverts Best Friend</Text>
             <Text>A FullStack Application by Grace & Kimberly</Text>
+            <Pressable onPress={() => navigation.navigate('Profile')}>
+                <Text>Profile</Text>
+            </Pressable>
+            <Pressable onPress={() => navigation.navigate('Calendar')}>
+                <Text>Calendar</Text>
+            </Pressable>
         </SafeAreaView>
     )
 }
