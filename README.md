5e93d020143bca0928ad5e4c27a57855ff91f21b
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-06-20 20:55:47 -0400

    changes to readme

--- README.md
+++ README.md
@@ -7,6 +7,8 @@
 
 PyJWT - PyJWT is a Python library which allows you to encode and decode JSON Web Tokens (JWT).
 
+Firebase for Auth
+
 <!-- hayden's relational table: -->
 <!-- followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('users.id')),
--- ios/Canceled/AppDelegate.mm
+++ ios/Canceled/AppDelegate.mm
@@ -2,17 +2,30 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <UIKit/UIKit.h>
+#import <FirebaseCore/FirebaseCore.h>
+
+
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-  self.moduleName = @"Canceled";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{};
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [FIRApp configure];
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  return YES;
 }
+
+//@implementation AppDelegate
+//
+//- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+//{
+//  self.moduleName = @"Canceled";
+//  // You can add your custom initial props in the dictionary below.
+//  // They will be passed down to the ViewController used by React Native.
+//  self.initialProps = @{};
+//
+//  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+//}
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
